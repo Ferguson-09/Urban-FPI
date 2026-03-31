4930ccb1d506f41989f8d653832a7f70fd517560
Ferguson Zibiri <excelzibiri5@gmail.com>
2026-03-30 20:50:21 -0500

    ferguson part of the file added(main page)

--- assets/scripts/first.js
+++ assets/scripts/first.js
@@ -4,6 +4,7 @@ const openPage = document.querySelector('.open');
 const overlay = document.querySelector('.overlay');
 const close = document.querySelector('.x');
 const input = document.querySelectorAll('input');
+const signIn = document.querySelector('.submit-btn')
 
 openPage.addEventListener('click', () => {
     overlay.style.display = 'grid'
@@ -13,3 +14,4 @@ close.addEventListener('click', () => {
     overlay.style.display = 'none';
     input.forEach(input => input.value = '');
 });
+
